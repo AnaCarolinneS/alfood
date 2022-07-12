d73e88c5b0b0e268ed0c04ef3e445af7f56c5c05
Ana Carolinne <38358019+AnaCarolinneS@users.noreply.github.com>
2022-07-11 21:45:06 -0300

    feature - Integração da API com Axios
    feature - Integração da API com Axios

---
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import IRestaurante from '../../interfaces/IRestaurante';
+import style from './ListaRestaurantes.module.scss';
+import Restaurante from './Restaurante';
+
+const ListaRestaurantes = () => {
+
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>()
+
+  useEffect(()=> {
+    //obter restaurantes
+    axios.get('http://localhost:8000/api/v1/restaurantes/')
+    .then(resposta => {
+      setRestaurantes(resposta.data.results) //veio do console do get
+    })
+    .catch(erro => {
+      console.log(erro)
+    })
+  }, []) //hook
+
+
+  return (<section className={style.ListaRestaurantes}>
+    <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+  </section>)
+}
+
+export default ListaRestaurantes
