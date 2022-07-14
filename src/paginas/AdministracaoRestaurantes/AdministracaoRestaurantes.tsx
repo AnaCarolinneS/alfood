e615ea44883a8cc64c76e23587663e6658d5bbe9
Ana Carolinne <38358019+AnaCarolinneS@users.noreply.github.com>
2022-07-14 11:32:30 -0300

    Implementando rotas na tela de admin
    Implementando rotas na tela de admin

--- src/paginas/AdministracaoRestaurantes/AdministracaoRestaurantes.tsx
+++ src/paginas/AdministracaoRestaurantes/AdministracaoRestaurantes.tsx
@@ -1,9 +1,10 @@
-import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { AppBar, Box, Button, Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Toolbar, Typography, Link } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import http from "../../http";
 import IRestaurante from "../../interfaces/IRestaurante";
+
+import { Link as RouterLink } from "react-router-dom"; //alterando o nome para nao gerar conflito
 
 const AdministracaoRestaurantes = () => { //arrow function
     const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
@@ -16,44 +17,79 @@ const AdministracaoRestaurantes = () => { //arrow function
     //O axios recebe como primeiro parâmetro o endereço para onde faremos a requisição. Já o segundo argumento é //um objeto literal que será transformado em json e enviado no corpo da requisição, dessa forma:
 
     const excluir = (restauranteExcluido: IRestaurante) => {
-     http.delete(`restaurantes/${restauranteExcluido.id}/`)
-     .then(() => {
-        const listaRestaurantesAtualizados = restaurantes.filter(restaurante => restaurante.id !== restauranteExcluido.id) //restaurantes que tenham o id diferente do que foi excluido!
-        setRestaurantes([...listaRestaurantesAtualizados])
-     })
+        http.delete(`restaurantes/${restauranteExcluido.id}/`)
+            .then(() => {
+                const listaRestaurantesAtualizados = restaurantes.filter(restaurante => restaurante.id !== restauranteExcluido.id) //restaurantes que tenham o id diferente do que foi excluido!
+                setRestaurantes([...listaRestaurantesAtualizados])
+            })
     }
     return (
 
-        <TableContainer component={Paper}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>
-                            Nome
-                        </TableCell>
-                        <TableCell>
-                            Editar
-                        </TableCell>
-                        <TableCell>
-                            Excluir
-                        </TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {restaurantes.map(restaurante => <TableRow key={restaurante.id}>
-                        <TableCell>
-                           {restaurante.nome}
-                        </TableCell>
-                        <TableCell>
-                           [<Link to={`/admin/restaurantes/${restaurante.id}`}> Editar </Link>]
-                        </TableCell>
-                        <TableCell>
-                         <Button variant="outlined" color="error" onClick={() => excluir(restaurante)}>Excluir</Button>
-                        </TableCell>
-                    </TableRow>)}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <>
+            <AppBar position="static">
+                <Container maxWidth='xl'>
+                    <Toolbar>
+                        <Typography variant="h6">
+                            Administração
+                        </Typography>
+                        <Box sx={{ display: 'flex', flexGrow: 1 }}>
+                            <Link component={RouterLink} to="/admin/restaurantes">
+                                <Button sx={{ my: 2, color: 'white' }}>
+                                    Restaurantes
+                                </Button>
+                            </Link>
+                            <Link component={RouterLink} to="/admin/restaurantes/novo">
+                                <Button sx={{ my: 2, color: 'white' }}>
+                                    Novo Restaurante
+                                </Button>
+                            </Link>
+                        </Box>
+                    </Toolbar>
+                </Container>
+
+            </AppBar>
+
+            <Box>
+                <Container maxWidth='lg' sx={{ marginTop: 1 }}>
+                    <Paper sx={{ p: 2 }}>
+                        {/*Conteudo da pagina*/}
+
+                        <TableContainer component={Paper}>
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>
+                                            Nome
+                                        </TableCell>
+                                        <TableCell>
+                                            Editar
+                                        </TableCell>
+                                        <TableCell>
+                                            Excluir
+                                        </TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {restaurantes.map(restaurante => <TableRow key={restaurante.id}>
+                                        <TableCell>
+                                            {restaurante.nome}
+                                        </TableCell>
+                                        <TableCell>
+                                            [<RouterLink to={`/admin/restaurantes/${restaurante.id}`}> Editar </RouterLink>]
+                                        </TableCell>
+                                        <TableCell>
+                                            <Button variant="outlined" color="error" onClick={() => excluir(restaurante)}>Excluir</Button>
+                                        </TableCell>
+                                    </TableRow>)}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Paper>
+                </Container>
+            </Box>
+
+        </>
+
     )
 }
 
